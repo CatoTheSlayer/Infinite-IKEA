479c6490f07d18654cfed89b980465563f43c571
Duen123 <katrine.due.spil@gmail.com>
2026-03-24 12:01:19 +0100

    UI

--- Infinite IKEA/Assets/Scripts/StartMenuEvents.cs
+++ Infinite IKEA/Assets/Scripts/StartMenuEvents.cs
@@ -4,25 +4,40 @@ using UnityEngine.SceneManagement;
 public class StartMenuEvents : MonoBehaviour
 {
    private UIDocument _dokument;
-
-    private Button _button;
+    //Referencer til de forskellige knapper på start siden
+    private Button _StartButton;
+    private Button _QuitButton;
 
     private void Awake()
     {
         _dokument = GetComponent<UIDocument>();
 
-        _button = _dokument.rootVisualElement.Q("StartSpilKnap") as Button;
-        _button.RegisterCallback<ClickEvent>(OnStartSpilClick);
+        //Laver start start knap, og for den til at køre metoden OnStartSpilClick når der trykkes på knappen Start
+        _StartButton = _dokument.rootVisualElement.Q("StartSpilKnap") as Button;
+        _StartButton.RegisterCallback<ClickEvent>(OnStartSpilClick);
+
+        //Sker det samme untagen den kører metoden OnQuitSpilClick når der trykkes på knappen Quit
+        _QuitButton = _dokument.rootVisualElement.Q("QuitButton") as Button;
+        _QuitButton.RegisterCallback<ClickEvent>(OnQuitSpilClick);
     }
 
     private void OnDisable()
     {
-        _button.UnregisterCallback<ClickEvent>(OnStartSpilClick);
+        _StartButton.UnregisterCallback<ClickEvent>(OnStartSpilClick);
+        _QuitButton.UnregisterCallback<ClickEvent>(OnQuitSpilClick);
     }
 
+
+    //
     private void OnStartSpilClick(ClickEvent evt)
     {
         Debug.Log("You trrykkede på start spil Knappen");
         SceneManager.LoadScene("LevelMenu");
+    }
+
+    private void OnQuitSpilClick(ClickEvent evt)
+    {
+        Debug.Log("You trrykkede på Quit spil Knappen");
+        Application.Quit();
     }
 }
