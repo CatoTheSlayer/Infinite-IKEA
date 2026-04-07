ff627def0eb2c8105c996353d785746de8f766b1
CatoTheSlayer <iancatohansen@gmail.com>
2026-04-07 10:16:56 +0200

    changes

--- Infinite IKEA/Assets/Scripts/CombatUI.cs
+++ Infinite IKEA/Assets/Scripts/CombatUI.cs
@@ -7,6 +7,7 @@ using UnityEngine.UIElements;
 
 public class CombatUI : MonoBehaviour
 {
+    private MainManager mainManager;
     private UIDocument document;
     private Button AttackButton;
     private Button ItemButton;
@@ -45,6 +46,12 @@ public class CombatUI : MonoBehaviour
         AttackButton.visible = false;
         ItemButton.visible = false;
         BackButton.visible = true;
+        //attactk 1 visble = true
+        Debug.Log("items in slot 0: " + mainManager.itemSlots[0]);
+        Debug.Log("item name in slot 0: " + mainManager.availableItems[mainManager.itemSlots[0]].itemName);
+        progressBar.value -= mainManager.availableItems[mainManager.itemSlots[0]].itemDamage; // Example of calculating damage and updating HP
+        Debug.Log("Damage dealt: " + mainManager.availableItems[mainManager.itemSlots[0]].itemDamage);
+        Debug.Log("Current HP: " + progressBar.value);
         //show a new ui that shows the weapons that you have equiped and then use mousePosition to select attack type
         //calculate the damage and then update the progress bar to show the new HP value of the target
         //then end the turn and go back to the old ui
@@ -70,10 +77,16 @@ public class CombatUI : MonoBehaviour
     }
     void Inventory()
     {
-        // Implement inventory logic here, using mousePosition for targeting if needed
+        // Implement inventory logic here, using mousePosition for targeting if needed'
+        inventoryContents = mainManager.availableItems[mainManager.itemSlots[0]].itemPreview; // Get the current inventory contents from the MainManager
         //hide the old ui
         //open a new ui where the player can see and interact with their inventory, using mousePosition to select items and manage them
         //if something is changed in the inventory then update the combat ui to show the new inventory and what is equiped
         //go bakc to the first ui
+    }
+    void Update()
+    {
+        ItemSlots = mainManager.itemSlots;
+        availableItems = mainManager.availableItems;
     }
 }
