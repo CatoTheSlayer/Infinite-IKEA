7704d147b0639a81626aed362d4a8a975c719fe0
SomeOne3333 <askhojmadsen@gmail.com>
2026-04-07 07:27:22 +0200

    pls virk

--- Infinite IKEA/Assets/Scripts/mision, colectoin.cs
+++ Infinite IKEA/Assets/Scripts/mision, colectoin.cs
@@ -1,14 +1,12 @@
 using UnityEngine;
 
-public class mision,colectoin : MonoBehaviour
+public class mision_colectoin : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
