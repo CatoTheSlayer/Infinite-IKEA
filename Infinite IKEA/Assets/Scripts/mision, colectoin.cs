d13e3aa6855fa4cbca631658242540e68d3494a8
SomeOne3333 <askhojmadsen@gmail.com>
2026-04-07 08:44:21 +0200

    juih

--- Infinite IKEA/Assets/Scripts/mision, colectoin.cs
+++ Infinite IKEA/Assets/Scripts/mision, colectoin.cs
@@ -2,13 +2,23 @@ using UnityEngine;
 
 public class mision_colectoin : MonoBehaviour
 {
+    public GameObject coin;
+    public Transform objects;
+    public float CoinHigt = 2f;
     void Start()
     {
-        
+        Colection();
     }
 
-    void Update()
+    void Colection()
     {
-        
+        foreach (Transform child in objects)
+        {
+            if (Random.value > 0.5f) // 50% chance
+            {
+                Vector3 spawnPos = child.position + Vector3.up * CoinHigt;
+                Instantiate(coin, spawnPos, Quaternion.identity);
+            }
+        }
     }
 }
